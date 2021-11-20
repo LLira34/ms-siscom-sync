0b43e35ab760dff728d5c4945d2006cf3d2c59f5
llira34 <luislira1029@gmail.com>
2021-11-20 17:49:56 -0600

    Add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* Populate tabla customers */
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Andrés', 'Guzmán', 'profesor@bolsadeideas.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Mr. John', 'Doe', 'john.doe@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Linus', 'Torvalds', 'linus.torvalds@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Rasmus', 'Lerdorf', 'rasmus.lerdorf@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Erich', 'Gamma', 'erich.gamma@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Richard', 'Helm', 'richard.helm@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Ralph', 'Johnson', 'ralph.johnson@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('John', 'Vlissides', 'john.vlissides@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Dr. James', 'Gosling', 'james.gosling@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Magma', 'Lee', 'magma.lee@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Tornado', 'Roe', 'tornado.roe@gmail.com', now());
+INSERT INTO customers (name, last_name, email, created_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', now());
