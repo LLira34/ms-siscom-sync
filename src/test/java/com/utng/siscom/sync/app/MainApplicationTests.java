3a9b6e5a0e7b84b50a54794adfa1201c30fdde71
llira34 <luislira1029@gmail.com>
2021-11-20 15:41:42 -0600

    Initial commit

---
+++ src/test/java/com/utng/siscom/sync/app/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utng.siscom.sync.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
